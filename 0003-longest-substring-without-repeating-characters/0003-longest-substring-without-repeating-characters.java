1f6097ca8dd314ff8c5cc858faa7ef5651adf45e
Vignesh Kandem <kandemvignesh@gmail.com>
2025-07-08 00:20:39 +0530

    Time: 6 ms (72.13%), Space: 45.1 MB (23.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,43 +1,5 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-/*
-        int maxLength = 0;
-         int n = s.length();
-
-         for(int i = 0;i<n;i++) 
-         {
-             boolean visited[] = new boolean[256];
-             for(int j = i;j<n;j++)
-             {
-                 if(visited[s.charAt(j)])
-                 {
-                     break;
-                 }
-                 visited[s.charAt(j)] = true;
-                 maxLength = Math.max(maxLength, j - i + 1);              
-             }
-          
-         }
-         return maxLength;
-        
-        
-         Map<Character, Integer> hash = new HashMap<>();
-         int n = s.length();
-         int l = 0;
-         int r = 0;
-         int maxLen = 0;
-         while(r<n){
-             if(hash.containsKey(s.charAt(r)) && hash.get(s.charAt(r)) >= l){
-                 l = hash.get(s.charAt(r)) + 1;
-             }
-             int len  = r-l+1;
-             maxLen = Math.max(len, maxLen);
-             hash.put(s.charAt(r), r);
-             r++;
-          }
-        return maxLen;
-*/     
-
      int n = s.length();
      int l = 0;
      int r = 0;
