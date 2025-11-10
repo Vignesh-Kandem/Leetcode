f414e013b25bd31fc64ee0706b5210adfddb9805
Vignesh Kandem <kandemvignesh@gmail.com>
2025-11-10 18:47:36 +0530

    Time: 8 ms (85.85%), Space: 49.3 MB (6.07%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,25 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intv) {
-         int n = intv.length;
-         List<int[]> res = new ArrayList<>();
-         Arrays.sort(intv, (a,b)->(a[0]-b[0]));
-         int pAt = intv[0][0];
-         int pDt = intv[0][1];
-         res.add(new int[]{pAt , pDt});
-         for(int i = 1;i<n;i++){
-              int cAt = intv[i][0];
-              int cDt = intv[i][1];
-              int last[] = res.get(res.size()-1);
-              if(cAt <= pDt) {
-                   last[1] = Math.max(last[1] , cDt);
-                   pDt = Math.max(last[1] , cDt);
-              }
-              else {
-                   res.add(new int[]{cAt ,cDt});
-                   pAt = cAt;
-                   pDt = cDt;
-              }
-         }
-         return res.toArray(new int[res.size()][2]);
+        int n=intv.length;
+        if(n==0) return new int[0][0];
+        Arrays.sort(intv,(a,b)->a[0]-b[0]);
+        List<int[]> res=new ArrayList<>();
+        res.add(new int[]{intv[0][0], intv[0][1]});
+        for(int i=1;i<n;i++){
+            int start=intv[i][0];
+            int end=intv[i][1];
+            int[] last=res.get(res.size()-1);
+            if(start<=last[1]){
+                last[1]=Math.max(last[1], end);
+            }
+            else{
+                res.add(new int[]{start, end});
+            }
+        }
+        return res.toArray(new int[res.size()][]);
     }
 }
