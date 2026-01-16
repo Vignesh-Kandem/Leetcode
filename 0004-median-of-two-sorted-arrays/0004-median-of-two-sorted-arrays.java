8d0011aad624fa6a5e7726c6e632d3165a5ecfe4
Vignesh Kandem <kandemvignesh@gmail.com>
2026-01-16 21:48:43 +0530

    Time: 1 ms (99.56%), Space: 48.9 MB (60.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,37 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int com[] = new int[m+n];
-        int p1 = 0;
-        int p2 = 0;
-        int k = 0;
-        while(p1 < m && p2<n){
-            if(nums1[p1] <= nums2[p2]){
-                com[k++] = nums1[p1++];
+        int n1=nums1.length, n2=nums2.length;
+        int i=0, j=0, count=0;
+        int n=(n1+n2);
+        int ind2=n/2, ind1=ind2-1;
+        int el1=-1, el2=-1;
+        while(i<n1 && j<n2) {
+            if(nums1[i]<nums2[j]) {
+                if(count==ind1) el1=nums1[i];
+                if(count==ind2) el2=nums1[i];
+                count++;
+                i++;
             }
-            else{
-                com[k++] = nums2[p2++];
+            else {
+                if(count==ind1) el1=nums2[j];
+                if(count==ind2) el2=nums2[j];
+                count++;
+                j++;
             }
         }
-        while(p1 < m){
-           com[k++] = nums1[p1++];
+        while(i<n1) {
+            if(count==ind1) el1=nums1[i];
+            if(count==ind2) el2=nums1[i];
+            count++;
+            i++;
         }
-        while(p2 < n){
-           com[k++] = nums2[p2++];
+        while(j<n2) {
+            if(count==ind1) el1=nums2[j];
+            if(count==ind2) el2=nums2[j];
+            count++;
+            j++;
         }
-        if((n+m)%2!=0){
-            return com[((n+m+1)/2)-1];
-        }
-         double res = ((com[(m+n)/2]+com[((m+n)/2)-1])/2.0);
-         return res;
+        if(n%2==1) return el2;
+        else return (el1+el2)/2.0;
     }
 }
