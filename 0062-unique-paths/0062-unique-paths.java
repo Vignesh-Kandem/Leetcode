d3e8925198d5a389bbbbe90264d475fc4d880b54
Vignesh Kandem <kandemvignesh@gmail.com>
2026-08-05 07:47:06 +0530

    Time: 1 ms (14.48%), Space: 42.1 MB (48.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,13 +1,73 @@
-class Solution {
+// Recursion.
+/* class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) dp[i][0] = 1;
-        for (int j = 0; j < n; j++) dp[0][j] = 1;
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        return helper(0,0,m,n);
+    }
+    public int helper(int i, int j, int m, int n) {
+        if(i==m-1 && j==n-1) return 1;
+        if(i>m-1 || j>n-1) return 0;
+        int right=helper(i,j+1,m,n);
+        int down=helper(i+1,j,m,n);
+        return right+down;
+    }
+} */
+
+// Memoization.
+/* class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        for(int[] i:dp) Arrays.fill(i,-1);
+        return helper(0,0,m,n,dp);
+    }
+    public int helper(int i, int j, int m, int n, int dp[][]) {
+        if(i==m-1 && j==n-1) return 1;
+        if(i>m-1 || j>n-1) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int right=helper(i,j+1,m,n,dp);
+        int down=helper(i+1,j,m,n,dp);
+        return dp[i][j]=right+down;
+    }
+} */
+
+// Tabulation.
+/* class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        dp[m-1][n-1]=1;
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>=0;j--) {
+                if(i==m-1 && j==n-1) continue;
+                int right=0;
+                int down=0;
+                if(j+1<n) right=dp[i][j+1];
+                if(i+1<m) down=dp[i+1][j];
+                dp[i][j]=right+down;
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[0][0];
+    }
+} */
+
+// Tabulation(With space optimization).
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int prev[]=new int[n];
+        prev[n-1]=1;
+        for(int i=m-1;i>=0;i--) {
+            int curr[]=new int[n];
+            for(int j=n-1;j>=0;j--) {
+                if(i==m-1 && j==n-1) {
+                    curr[j]=1;
+                    continue;
+                }
+                int right=0;
+                int down=0;
+                if(j+1<n) right=curr[j+1];
+                down=prev[j];
+                curr[j]=right+down;
+            }
+            prev=curr;
+        }
+        return prev[0];
     }
 }
